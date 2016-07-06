c4856d85d117dc9ebdaa60a3aa7ba2bcc836759d
Khaled <khaledsaikat@gmail.com>
2016-07-06 14:40:42 +0200

    lang support

--- controllers/GeneralController.php
+++ controllers/GeneralController.php
@@ -12,24 +12,24 @@ class GeneralController
 
     public function __construct()
     {
-        add_shortcode('code', array($this, 'shortcode'));
-        add_shortcode('code-viewer', array($this, 'shortcode'));
+        add_shortcode('code-viewer', array(
+            $this,
+            'shortcode'
+        ));
     }
 
     public function shortcode($atts, $content = null)
     {
-        if (array_key_exists('src', $atts)) {
-            $url = $atts['src'];
-            $code = file_get_contents($url);
-        } else {
-            $code = $content;
-        }
+        extract(shortcode_atts(array(
+            'src' => null,
+            'lang' => 'empty'
+        ), $atts, 'code-viewer'));
+        $source = $src ? file_get_contents($src) : $content;
         
-        $language = 'php';
-        $geshi = new \GeSHi($code, $language);
+        $geshi = new \GeSHi($source, $lang);
         $geshi->enable_line_numbers(GESHI_FANCY_LINE_NUMBERS, 2);
         $geshi->set_line_style('background: #fcfcfc;', 'background: #f0f0f0;');
-        $geshi->set_code_style('font-size: 12px;');
+        
         return $geshi->parse_code();
     }
 }
